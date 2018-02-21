22273337e8b2295937874073f532d74498857aa2
Dixon Urbina <durbina@loto.com.ni>
2018-02-21 16:58:25 -0600

    asfd

--- Controller/LoadController.php
+++ Controller/LoadController.php
@@ -1,17 +1,15 @@
 <?php 
 class LoadController{
+
     public function __construct(){
             include("UserController.php");
-            include("Model/Entity/Conect.php");
-            $conect = new Conect();
-            $conect->__SET('user', $_REQUEST('user'));
-            $conect->__SET('pass', $_REQUEST('pass'));
             Define("_USER", $_REQUEST('user'));
-            define("_PASS", $_REQUEST('pass'));
+            define("_PASS", $_REQUEST('pass'));  
+            
     }
 
     public function load(){
-        
+
     }
 }
 ?>
