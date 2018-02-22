b5525396803f058e551ff1370fc5b1c047c895ec
Lucila Izaguirre <lizaguirre@loto.com.ni>
2018-02-22 09:04:52 -0600

    Verificar que sube cambios

--- Model/Entity/Conect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    class Conect{
-        private $user, $pass;
-        public function __SET($var, $val){ $this->var = $val }
-        public function __GET($var){ return $this->var}
-    }
-?>
